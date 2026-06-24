0b571acea12c829bd627af989243dd82fef50b69
Faustino Cedeño Berroa <fcedenob1@gmail.com>
2026-06-24 03:08:46 -0400

    Add plate validation functionality
    Implement main function to validate license plates.

---
+++ test_plates/plates.py
@@ -0,0 +1,33 @@
+def main():
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+def is_valid(s):
+    if len(s) < 2 or len(s) > 6:
+        return False
+
+    if not s.isalnum():
+        return False
+
+    if not s[0].isalpha() or not s[1].isalpha():
+        return False
+
+    numero_iniciado = False
+    for c in s:
+        if c.isdigit():
+            if not numero_iniciado and c == "0":
+                return False
+            numero_iniciado = True
+        else:
+            if numero_iniciado:
+                return False
+
+    return True
+
+
+if __name__ == "__main__":
+    main()
