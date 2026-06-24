2639acb8bb2f05db9d264e35cda5d435783a1ca8
Faustino Cedeño Berroa <fcedenob1@gmail.com>
2026-06-24 02:56:57 -0400

    Add meal time notifications based on user input

---
+++ meal/meal.py
@@ -0,0 +1,17 @@
+def main():
+    time = convert(input("What time is it? "))
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
+
+
+def convert(time):
+    hours, minutes = time.split(":")
+    return int(hours) + int(minutes) / 60
+
+
+if __name__ == "__main__":
+    main()
